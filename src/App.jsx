f48595574fafba37c487aa5c0888c35eb52e78d2
unknown <mdtanvir7462@gmail.com>
2023-04-26 14:21:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { NavLink } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+     <ul>
+      <li><NavLink to='/'>home</NavLink></li>
+     </ul>
+    </>
+  )
+}
+
+export default App
